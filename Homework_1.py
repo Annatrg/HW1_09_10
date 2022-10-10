eb97f4ce1f6db9a16425722777f12cd1f256f9e8
Annatrg <torgova-anna@mail.ru>
2022-10-10 23:44:41 +0300

    Созданы вспомогательные классы

--- Homework_1.py
+++ Homework_1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from group import Group
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox(firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe")
@@ -17,7 +18,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
-    def create_group(self, wd, name_group, header_group, footer_group):
+    def create_group(self, wd, group):
         # open groups page
         wd.find_element_by_link_text("groups").click()
         # init group creation
@@ -25,11 +26,11 @@ class TestAddGroup(unittest.TestCase):
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name_group)
+        wd.find_element_by_name("group_name").send_keys(group.name_group)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header_group)
+        wd.find_element_by_name("group_header").send_keys(group.header_group)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer_group)
+        wd.find_element_by_name("group_footer").send_keys(group.footer_group)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -43,7 +44,7 @@ class TestAddGroup(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
-        self.create_group(wd, name_group="Homework_1_2", header_group="homework", footer_group="home")
+        self.create_group(wd, Group(name_group="Homework_1_2", header_group="homework", footer_group="home"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
