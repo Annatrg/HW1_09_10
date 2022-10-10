664781deec10ffbdc2d08011f30d0d731624643a
Annatrg <torgova-anna@mail.ru>
2022-10-10 23:37:17 +0300

    Реализована передача данных пользователя и данных о создаваемой группе в виде параметров

--- Homework_1.py
+++ Homework_1.py
@@ -9,15 +9,15 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("https://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, user_name, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(user_name)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name_group, header_group, footer_group):
         # open groups page
         wd.find_element_by_link_text("groups").click()
         # init group creation
@@ -25,11 +25,11 @@ class TestAddGroup(unittest.TestCase):
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Homework_1_2")
+        wd.find_element_by_name("group_name").send_keys(name_group)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("homework")
+        wd.find_element_by_name("group_header").send_keys(header_group)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("home")
+        wd.find_element_by_name("group_footer").send_keys(footer_group)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -42,8 +42,8 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_group(wd)
+        self.login(wd, user_name="admin", password="secret")
+        self.create_group(wd, name_group="Homework_1_2", header_group="homework", footer_group="home")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
