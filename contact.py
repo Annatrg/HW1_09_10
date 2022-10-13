e0dd91d6446db6ae50ef2364067d7ee4f6751e96
Annatrg <torgova-anna@mail.ru>
2022-10-13 22:46:39 +0300

    1. Изменено название класса (без _) 2. Переименованы переменные (с маленькой буквы)

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, first_name, last_name, address, mobile_phone, email, day_of_birth, month_of_birth,
+                        year_of_birth):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.mobile_phone = mobile_phone
+        self.email = email
+        self.day_of_birth = day_of_birth
+        self.month_of_birth = month_of_birth
+        self.year_of_birth = year_of_birth
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -83,3 +83,4 @@ class TestAddContact(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
