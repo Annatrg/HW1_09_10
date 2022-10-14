ec7f9154d4a09fe814b1b8ea5b84345f6e0af7cd
Annatrg <torgova-anna@mail.ru>
2022-10-14 22:28:41 +0300

    Перевод на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    def test_add_contact(self):
-        self.app.login(login="admin", password="secret")
-        self.app.add_new_contact(Contact(first_name="Anna", last_name="Torgova", address="Spb", mobile_phone="79657989864", email="torgova-anna@mail.ru", day_of_birth="26", month_of_birth="November",
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+    app.login(login="admin", password="secret")
+    app.add_new_contact(Contact(first_name="Anna", last_name="Torgova", address="Spb", mobile_phone="79657989864", email="torgova-anna@mail.ru", day_of_birth="26", month_of_birth="November",
                              year_of_birth="1996"))
-        self.app.logout()
+    app.logout()
 
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
 
